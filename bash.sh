e758a92b2d77ea30ccf93325cbd69cac79d49efe
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-08 12:34:16 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/Web4application/Cache.git
+cd Cache
+
+# Add the new files here (main.go, cache.go, api.go, etc.)
+git add .
+git commit -m "Add full caching microservice with LRU, TTL, API, persistence"
+git push

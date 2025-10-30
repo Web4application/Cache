074d7d425005c0d1300ba3b515f1c161dde11e7c
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-30 12:00:32 +0100

    Create install.sh

---
+++ .vim/install.sh
@@ -0,0 +1,6 @@
+$ mkdir -p ~/.vim/pack/bundle/start/vim-rzip
+$ unzip ~/Downloads/vim-rzip.zip -d ~/.vim/pack/bundle/start/vim-rzip
+
+$ mkdir -p ~/.vim/pack/bundle/start
+$ cd ~/.vim
+.vim $ git submodule add 'https://github.com/lbrayner/vim-rzip' pack/bundle/start/vim-rzip
